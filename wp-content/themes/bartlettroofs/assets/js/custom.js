b421fdf3e1acef93b7f54c032a7cb77417c2ee26
jrixgeromo <jrixgeromo@gmail.com>
2022-09-12 08:55:31 +0800

    updates

--- wp-content/themes/bartlettroofs/assets/js/custom.js
+++ wp-content/themes/bartlettroofs/assets/js/custom.js
@@ -1,9 +1,9 @@
 if($("#page-selected").val() == "single") {
 	$( "#main-body" ).removeClass("home-page")
 	$( "#main-body" ).removeClass("transparent")
-	// $( "#main-body" ).removeClass("toplevel-page")
+	$( "#main-body" ).removeClass("toplevel-page")
 } else {
 	$( "#main-body" ).addClass("home-page")
 	$( "#main-body" ).addClass("transparent")
-	// $( "#main-body" ).addClass("toplevel-page")
+	$( "#main-body" ).addClass("toplevel-page")
 }
