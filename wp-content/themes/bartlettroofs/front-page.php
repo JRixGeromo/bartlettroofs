4e9c119d662c2611ec9462afa708daaeceb59b26
jrixgeromo <jrixgeromo@gmail.com>
2022-09-12 10:42:01 +0800

    updates

--- wp-content/themes/bartlettroofs/front-page.php
+++ wp-content/themes/bartlettroofs/front-page.php
@@ -695,33 +695,9 @@
                   <a href="service-areas/index.html" class="btn btn-info btn-lg btn-padding">View More</a>
                </div>
             </div>
-            <div class="row scroll-row mt-5 text-center triggerAnimate fadeInUp delay-4ms">
-               <a href="service-areas/index.html#idaho" class="col-md-2 areas scroll-to">
-                  <img class="img-responsive hvr-float center-block lazyload match-height" src="<?php echo get_stylesheet_directory_uri();?>/assets/images/ph-rectangle.png" data-src="<?php echo get_stylesheet_directory_uri();?>/assets/images/home/idaho.svg" alt="Idaho">
-                  <p class="h4">Idaho</p>
-               </a>
-               <a href="service-areas/index.html#washington"  class="col-md-2 areas scroll-to">
-                  <img class="img-responsive hvr-float center-block lazyload match-height" src="<?php echo get_stylesheet_directory_uri();?>/assets/images/ph-rectangle.png" data-src="<?php echo get_stylesheet_directory_uri();?>/assets/images/home/washington.svg" alt="Washington">
-                  <p class="h4">Washington</p>
-               </a>
-               <a href="service-areas/index.html#utah"  class="col-md-2 areas scroll-to">
-                  <img class="img-responsive hvr-float center-block lazyload match-height" src="<?php echo get_stylesheet_directory_uri();?>/assets/images/ph-rectangle.png" data-src="<?php echo get_stylesheet_directory_uri();?>/assets/images/home/utah.svg" alt="Utah">
-                  <p class="h4">Utah</p>
-               </a>
-               <a href="service-areas/index.html#montana" class="col-md-2 areas scroll-to">
-                  <img class="img-responsive hvr-float center-block lazyload match-height" src="<?php echo get_stylesheet_directory_uri();?>/assets/images/ph-rectangle.png" data-src="<?php echo get_stylesheet_directory_uri();?>/assets/images/home/montana.svg" alt="Montana">
-                  <p class="h4 scroll-to">Montana</p>
-               </a>
-               <a href="service-areas/index.html#wyoming" class="col-md-2 areas scroll-to">
-                  <img class="img-responsive hvr-float center-block lazyload match-height" src="<?php echo get_stylesheet_directory_uri();?>/assets/images/ph-rectangle.png" data-src="<?php echo get_stylesheet_directory_uri();?>/assets/images/home/wyoming.svg" alt="Idaho">
-                  <p class="h4 scroll-to">Wyoming</p>
-               </a>
-               <a href="service-areas/index.html#nevada" class="col-md-2 areas scroll-to">
-                  <img class="img-responsive hvr-float center-block lazyload match-height" src="<?php echo get_stylesheet_directory_uri();?>/assets/images/ph-rectangle.png" data-src="<?php echo get_stylesheet_directory_uri();?>/assets/images/home/nevada.svg" alt="Nevada">
-                  <p class="h4 scroll-to">Nevada</p>
-                  <p class="soon-text">Coming Soon!</p>
-               </a>
-            </div>
+            <?php
+               get_template_part('template-parts/main-service-areas-map-section','main-service-areas-map-section');
+            ?>
          </div>
       </section>
       <?php
---
+++ wp-content/themes/bartlettroofs/template-parts/main-service-areas-map-section.php
@@ -0,0 +1,27 @@
+<div class="row scroll-row mt-5 text-center triggerAnimate fadeInUp delay-4ms">
+   <a href="/bartlettroofs/offers/service-areas/#idaho" class="col-md-2 areas scroll-to">
+      <img class="img-responsive hvr-float center-block lazyload match-height" src="<?php echo get_stylesheet_directory_uri();?>/assets/images/ph-rectangle.png" data-src="<?php echo get_stylesheet_directory_uri();?>/assets/images/home/idaho.svg" alt="Idaho">
+      <p class="h4">Idaho</p>
+   </a>
+   <a href="/bartlettroofs/offers/service-areas/#washington"  class="col-md-2 areas scroll-to">
+      <img class="img-responsive hvr-float center-block lazyload match-height" src="<?php echo get_stylesheet_directory_uri();?>/assets/images/ph-rectangle.png" data-src="<?php echo get_stylesheet_directory_uri();?>/assets/images/home/washington.svg" alt="Washington">
+      <p class="h4">Washington</p>
+   </a>
+   <a href="/bartlettroofs/offers/service-areas/#utah"  class="col-md-2 areas scroll-to">
+      <img class="img-responsive hvr-float center-block lazyload match-height" src="<?php echo get_stylesheet_directory_uri();?>/assets/images/ph-rectangle.png" data-src="<?php echo get_stylesheet_directory_uri();?>/assets/images/home/utah.svg" alt="Utah">
+      <p class="h4">Utah</p>
+   </a>
+   <a href="/bartlettroofs/offers/service-areas/#montana" class="col-md-2 areas scroll-to">
+      <img class="img-responsive hvr-float center-block lazyload match-height" src="<?php echo get_stylesheet_directory_uri();?>/assets/images/ph-rectangle.png" data-src="<?php echo get_stylesheet_directory_uri();?>/assets/images/home/montana.svg" alt="Montana">
+      <p class="h4 scroll-to">Montana</p>
+   </a>
+   <a href="/bartlettroofs/offers/service-areas/#wyoming" class="col-md-2 areas scroll-to">
+      <img class="img-responsive hvr-float center-block lazyload match-height" src="<?php echo get_stylesheet_directory_uri();?>/assets/images/ph-rectangle.png" data-src="<?php echo get_stylesheet_directory_uri();?>/assets/images/home/wyoming.svg" alt="Idaho">
+      <p class="h4 scroll-to">Wyoming</p>
+   </a>
+   <a href="/bartlettroofs/offers/service-areas/#nevada" class="col-md-2 areas scroll-to">
+      <img class="img-responsive hvr-float center-block lazyload match-height" src="<?php echo get_stylesheet_directory_uri();?>/assets/images/ph-rectangle.png" data-src="<?php echo get_stylesheet_directory_uri();?>/assets/images/home/nevada.svg" alt="Nevada">
+      <p class="h4 scroll-to">Nevada</p>
+      <p class="soon-text">Coming Soon!</p>
+   </a>
+</div>
